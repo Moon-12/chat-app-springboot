03ab2a92e3f186ebc33147755504f67b10c293b7
Moon-12 <ashwijanayak@gmail.com>
2025-04-21 20:28:57 -0500

    #102 chat service

---
+++ src/main/java/dev/bedesi/sms/samplemanagementsystem/service/ChatService.java
@@ -0,0 +1,27 @@
+package dev.bedesi.sms.samplemanagementsystem.service;
+import dev.bedesi.sms.samplemanagementsystem.mysql.entity.ChatEntity;
+import dev.bedesi.sms.samplemanagementsystem.mysql.repository.ChatRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ChatService {
+
+    @Autowired
+    private ChatRepository chatRepository;
+
+
+    public Optional<List<ChatEntity>> getAllChatEntities() {
+        return chatRepository.findAllActive();
+    }
+
+    public ChatEntity createChatEntity(ChatEntity ChatEntity) {
+        return chatRepository.save(ChatEntity);
+    }
+
+
+
+}
