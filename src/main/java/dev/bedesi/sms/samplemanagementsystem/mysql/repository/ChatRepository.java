31ea405feb2124a18211b35fc0548b7b7f670c88
Moon-12 <ashwijanayak@gmail.com>
2025-04-21 20:28:34 -0500

    #101 chat repository

---
+++ src/main/java/dev/bedesi/sms/samplemanagementsystem/mysql/repository/ChatRepository.java
@@ -0,0 +1,13 @@
+package dev.bedesi.sms.samplemanagementsystem.mysql.repository;
+
+import dev.bedesi.sms.samplemanagementsystem.mysql.entity.ChatEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ChatRepository extends JpaRepository<ChatEntity,Integer> {
+    @Query("SELECT * FROM CHAT_MESSAGE c WHERE c.active = true")
+    Optional<List<ChatEntity>> findAllActive();
+}
