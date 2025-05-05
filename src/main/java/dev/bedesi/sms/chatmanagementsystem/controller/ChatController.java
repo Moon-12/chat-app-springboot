9196fc5dc2d18203d27d0ec5fa34140659b08bbe
Moon-12 <ashwijanayak@gmail.com>
2025-05-04 19:02:20 -0500

    cross origin header added

--- src/main/java/dev/bedesi/sms/chatmanagementsystem/controller/ChatController.java
+++ src/main/java/dev/bedesi/sms/chatmanagementsystem/controller/ChatController.java
@@ -4,25 +4,39 @@ import dev.bedesi.sms.chatmanagementsystem.dto.ChatDTO;
 import dev.bedesi.sms.chatmanagementsystem.mysql.entity.ChatEntity;
 import dev.bedesi.sms.chatmanagementsystem.service.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("chat")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ChatController {
     @Autowired
     ChatService chatService;
 
     @GetMapping("/getAllPreviousMessages")
-    public Optional<List<ChatDTO>> getAllChatEntities() {
-        return chatService.getAllChatEntities();
+    public ResponseEntity<?> getAllChatEntities() {
+        Optional<List<ChatDTO>> chatDTOList= chatService.getAllChatEntities();
+        if (chatDTOList.isPresent()) {
+            Map<String, Object> response = new HashMap<>();
+            response.put("data",chatDTOList);
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping("/postMessage")
-    public ChatDTO createSampleEntity(@RequestBody ChatEntity chatEntity) {
-        return chatService.createChatEntity(chatEntity);
+    public ResponseEntity<?> createSampleEntity(@RequestBody ChatEntity chatEntity) {
+        ChatDTO chatDTO=chatService.createChatEntity(chatEntity);
+        Map<String, Object> response = new HashMap<>();
+        response.put("data",chatDTO);
+        return ResponseEntity.ok(response);
     }
 
 
