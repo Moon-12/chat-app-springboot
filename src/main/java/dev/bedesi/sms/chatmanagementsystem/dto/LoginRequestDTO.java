aecb6c2f64f233fea17b8badabb46fccccfb9a28
Moon-12 <ashwijanayak@gmail.com>
2025-05-20 17:38:00 -0400

    login controller added , glocalcorsconfig file and loginDTO created

---
+++ src/main/java/dev/bedesi/sms/chatmanagementsystem/dto/LoginRequestDTO.java
@@ -0,0 +1,14 @@
+package dev.bedesi.sms.chatmanagementsystem.dto;
+
+
+public  class LoginRequestDTO {
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
